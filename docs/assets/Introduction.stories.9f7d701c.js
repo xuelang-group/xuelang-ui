94f840143f77366dd0855794a675ebe640732ab2
magic-zhu <18817313320@163.com>
2022-12-07 10:21:31 +0800

    ✨ github pages

---
+++ docs/assets/Introduction.stories.9f7d701c.js
@@ -0,0 +1,102 @@
+var p=Object.defineProperty;var a=(o,s)=>p(o,"name",{value:s,configurable:!0});import"./jsx-runtime.43400d81.js";import{c as t,A as c,M as m}from"./Props.e69a12fa.js";import"./iframe.ec1a40cb.js";import"./string.878f69fc.js";import"./es.map.constructor.2c5032a5.js";import"./es.number.to-fixed.edb62925.js";const d="/assets/code-brackets.9ef6443e.svg",g="/assets/colors.ac9401f3.svg",u="/assets/comments.f15a6837.svg",f="/assets/direction.94a9917f.svg",h="/assets/flow.275142c6.svg",b="/assets/plugin.57148314.svg",x="/assets/repo.fb4ece47.svg",k="/assets/stackalt.2ad81543.svg";function i(){return i=Object.assign?Object.assign.bind():function(o){for(var s=1;s<arguments.length;s++){var r=arguments[s];for(var n in r)Object.prototype.hasOwnProperty.call(r,n)&&(o[n]=r[n])}return o},i.apply(this,arguments)}a(i,"_extends");const y={},w="wrapper";function l({components:o,...s}){return t(w,i({},y,s,{components:o,mdxType:"MDXLayout"}),t(m,{title:"Example/Introduction",mdxType:"Meta"}),t("style",null,`
+    .subheading {
+      --mediumdark: '#999999';
+      font-weight: 900;
+      font-size: 13px;
+      color: #999;
+      letter-spacing: 6px;
+      line-height: 24px;
+      text-transform: uppercase;
+      margin-bottom: 12px;
+      margin-top: 40px;
+    }
+
+    .link-list {
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-template-rows: 1fr 1fr;
+      row-gap: 10px;
+    }
+
+    @media (min-width: 620px) {
+      .link-list {
+        row-gap: 20px;
+        column-gap: 20px;
+        grid-template-columns: 1fr 1fr;
+      }
+    }
+
+    @media all and (-ms-high-contrast:none) {
+    .link-list {
+        display: -ms-grid;
+        -ms-grid-columns: 1fr 1fr;
+        -ms-grid-rows: 1fr 1fr;
+      }
+    }
+
+    .link-item {
+      display: block;
+      padding: 20px 30px 20px 15px;
+      border: 1px solid #00000010;
+      border-radius: 5px;
+      transition: background 150ms ease-out, border 150ms ease-out, transform 150ms ease-out;
+      color: #333333;
+      display: flex;
+      align-items: flex-start;
+    }
+
+    .link-item:hover {
+      border-color: #1EA7FD50;
+      transform: translate3d(0, -3px, 0);
+      box-shadow: rgba(0, 0, 0, 0.08) 0 3px 10px 0;
+    }
+
+    .link-item:active {
+      border-color: #1EA7FD;
+      transform: translate3d(0, 0, 0);
+    }
+
+    .link-item strong {
+      font-weight: 700;
+      display: block;
+      margin-bottom: 2px;
+    }
+
+    .link-item img {
+      height: 40px;
+      width: 40px;
+      margin-right: 15px;
+      flex: none;
+    }
+
+    .link-item span {
+      font-size: 14px;
+      line-height: 20px;
+    }
+
+    .tip {
+      display: inline-block;
+      border-radius: 1em;
+      font-size: 11px;
+      line-height: 12px;
+      font-weight: 700;
+      background: #E7FDD8;
+      color: #66BF3C;
+      padding: 4px 12px;
+      margin-right: 10px;
+      vertical-align: top;
+    }
+
+    .tip-wrapper {
+      font-size: 13px;
+      line-height: 20px;
+      margin-top: 40px;
+      margin-bottom: 40px;
+    }
+
+    .tip-wrapper code {
+      font-size: 12px;
+      display: inline-block;
+    }
+  `),t("h1",null,"Welcome to \u96EA\u6D6A\u4E91 UI"),t("p",null,"\u96EA\u6D6A\u4E91 \u901A\u7528 UI"),t("div",{className:"subheading"},"Configure"),t("div",{className:"link-list"},t("a",{className:"link-item",href:"https://storybook.js.org/docs/react/addons/addon-types",target:"_blank"},t("img",{src:b,alt:"plugin"}),t("span",null,t("strong",null,"Presets for popular tools"),"Easy setup for TypeScript, SCSS and more.")),t("a",{className:"link-item",href:"https://storybook.js.org/docs/react/configure/webpack",target:"_blank"},t("img",{src:k,alt:"Build"}),t("span",null,t("strong",null,"Build configuration"),"How to customize webpack and Babel")),t("a",{className:"link-item",href:"https://storybook.js.org/docs/react/configure/styling-and-css",target:"_blank"},t("img",{src:g,alt:"colors"}),t("span",null,t("strong",null,"Styling"),"How to load and configure CSS libraries")),t("a",{className:"link-item",href:"https://storybook.js.org/docs/react/get-started/setup#configure-storybook-for-your-stack",target:"_blank"},t("img",{src:h,alt:"flow"}),t("span",null,t("strong",null,"Data"),"Providers and mocking for data libraries"))),t("div",{className:"subheading"},"Learn"),t("div",{className:"link-list"},t("a",{className:"link-item",href:"https://storybook.js.org/docs",target:"_blank"},t("img",{src:x,alt:"repo"}),t("span",null,t("strong",null,"Storybook documentation"),"Configure, customize, and extend")),t("a",{className:"link-item",href:"https://storybook.js.org/tutorials/",target:"_blank"},t("img",{src:f,alt:"direction"}),t("span",null,t("strong",null,"In-depth guides"),"Best practices from leading teams")),t("a",{className:"link-item",href:"https://github.com/storybookjs/storybook",target:"_blank"},t("img",{src:d,alt:"code"}),t("span",null,t("strong",null,"GitHub project"),"View the source and add issues")),t("a",{className:"link-item",href:"https://discord.gg/storybook",target:"_blank"},t("img",{src:u,alt:"comments"}),t("span",null,t("strong",null,"Discord chat"),"Chat with maintainers and the community"))),t("div",{className:"tip-wrapper"},t("span",{className:"tip"},"Tip"),"Edit the Markdown in"," ",t("code",null,"stories/Introduction.stories.mdx")))}a(l,"MDXContent");l.isMDXComponent=!0;const v=a(()=>{throw new Error("Docs-only story")},"__page");v.parameters={docsOnly:!0};const e={title:"Example/Introduction",includeStories:["__page"]},_={};e.parameters=e.parameters||{};e.parameters.docs={...e.parameters.docs||{},page:()=>t(c,{mdxStoryNameToKey:_,mdxComponentAnnotations:e},t(l,null))};const z=["__page"];export{z as __namedExportsOrder,v as __page,e as default};
+//# sourceMappingURL=Introduction.stories.9f7d701c.js.map
---
+++ docs/assets/config.ad70d5b3.js
@@ -0,0 +1,2 @@
+import{d as t,r as m,a as s}from"./decorateStory.07085385.js";import"./iframe.ec1a40cb.js";import"./vue.esm-bundler.18dcd8e3.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
+//# sourceMappingURL=config.ad70d5b3.js.map
